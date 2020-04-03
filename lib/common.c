3eed8a0530e120faea400f7ab397623b04e79ee6
feilaila <feilaila@126.com>
2020-04-03 16:37:53 +0800

    common

--- lib/common.c
+++ lib/common.c
@@ -21,3 +21,126 @@ char * sock_ntop(const struct sockaddr *sa,socklen_t *len){
         break;
     }
 }
+
+ssize_t readn(int fd,void *buff,size_t len){
+    ssize_t nread;
+    ssize_t nleft;
+    char *ptr;
+    nleft=len;
+    ptr=buff;
+
+    while(nleft>0){
+        nread=read(fd,ptr,nleft);
+        if(nread<0){
+            if(errno==EINTR){
+               continue;
+            }else
+            {
+                return -1;
+            }
+        }else if (nread == 0)
+        {
+            break;
+        }
+        
+        nleft -=nread;
+        ptr +=nread;
+    }
+    return len -nleft;
+}
+
+ssize_t writen(int fd,const void *buff,size_t len){
+    ssize_t nleft;
+    ssize_t nwrite;
+    char *ptr;
+    ptr = buff;
+    nleft = len;
+    while (nleft>0)
+    {
+        nwrite = write(fd,ptr,nleft);
+        if(nwrite<0 && errno == EINTR){continue;}
+        if(nwrite<0){return -1;}
+        ptr +=nwrite;
+        nleft -=nwrite;
+    }
+    return len - nleft;
+}
+
+ssize_t readline(int fd,void *buff,size_t len){
+    char c;
+    ssize_t i,rc;
+    char *ptr = buff;
+    for(i=0;i<len;i++){
+        rc=read(fd,&c,1);
+        if(rc==1){
+            ptr[i]=c;
+            if(c=='\n'){ptr[i]=0;return i;}
+        }else if(rc==0){
+            ptr[i]=0;
+            return i;
+        }else if (rc<0 && errno==EINTR)
+        {
+            continue;
+        }else
+        {
+            return -1;
+        }
+    }
+    
+}
+
+static int readcnt;
+static char *readptr;
+static char readbuf[2048];
+
+static ssize_t my_read(int fd,char *ptr){
+
+    if(readcnt<=0){
+        while (readcnt<0)
+        {
+            readcnt = read(fd,readbuf,sizeof(readbuf));
+            if(readcnt==0){
+                return 0;
+            }else if (readcnt<0 && errno==EINTR)
+            {
+                continue;
+            }else if (readcnt<0)
+            {
+                return -1;
+            }
+            readptr=readbuf;
+        }
+        
+    }
+    readcnt--;
+    *ptr = *readptr++;
+    return 1;
+}
+
+ssize_t my_readline(int fd,void *buff,size_t len){
+    char c;
+    ssize_t i,rc;
+    char *ptr = buff;
+    for(i=0;i<len;i++){
+        rc=my_read(fd,&c);
+        if(rc==1){
+            ptr[i]=c;
+            if(c=='\n'){ptr[i]=0;return i;}
+        }else if(rc==0){
+            ptr[i]=0;
+            return i;
+        }else if (rc<0 && errno==EINTR)
+        {
+            continue;
+        }else
+        {
+            return -1;
+        }
+    }
+}
+ssize_t my_readbuf(void **ptrptr){
+    if(readcnt){
+        *ptrptr = readptr;
+    }
+    return readcnt;
+}
--- lib/common.h
+++ lib/common.h
@@ -3,4 +3,19 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
 char *sock_ntop(const struct sockaddr *sockaddr,socklen_t *addrlen);
+
+int sock_bind_wild(int sockfd,int family);
+int sock_cmp_addr(const struct sockaddr *addr1,const struct sockaddr *addr2,socklen_t addrlen);
+int sock_cmp_port(const struct sockaddr *addr1,const struct sockaddr *addr2,socklen_t addrlen);
+int sock_get_port(const struct sockaddr *addr,socklen_t addrlen);
+char *sock_ntop_host(const struct sockaddr *addr,socklen_t addrlen);
+void sock_set_addr(const struct sockaddr *addr,socklen_t addrlen,void *ptr);
+void sock_set_port(const struct sockaddr *addr,socklen_t addrlen,int port);
+void sock_set_wild(const struct sockaddr *addr,socklen_t addrlen);
+
+ssize_t readn(int fd,void *ptr,size_t len);
+ssize_t writen(int fd,const void *buff,size_t len);
+ssize_t readline(int fd,void *buff,size_t len);
